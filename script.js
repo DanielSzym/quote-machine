9d06428d1575110b3933d2fbf2eb973bb6586ade
Daniel <enticr@gmx.de>
2018-02-24 22:17:37 +0100

    Added jQuery and ajax src to html

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
 
-})
+$(document).ready(function(){
+    $("#quote").on("click", function(){
+        giveQuote();
+    });
+});
+
+function giveQuote(){
+    //$("#text").html("jo");
+    $.getJSON("https://www.freecodecamp.org/json/cats.json", function(data){
+        $("#text").html(JSON.stringify(data));
+    });              
+}
